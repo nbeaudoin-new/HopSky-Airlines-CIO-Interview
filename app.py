439ba2597918ffccb143e0ff94672332881e5bd0
nbeaudoin-new <nick.s.beaudoin@gmail.com>
2025-12-22 12:58:53 -0800

    Fix LangChain imports: use langchain_core.messages and add langchain-core dependency

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from langchain_openai import ChatOpenAI
-from langchain.schema import HumanMessage, SystemMessage, AIMessage
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 from dotenv import load_dotenv
 import os
 
